45d07e87416b000332f3ba6d770e92a2e99f10b1
unknown <ericksumague7@gmail.com>
2022-09-17 23:46:07 +0800

    lightmode

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-const header = document.querySelector("header");
+const body = document.querySelector("body"),
+      modeToggle = document.querySelector(".dark-light");
+
+	  let getMode = localStorage.getItem("mode");
+          if(getMode && getMode === "dark-mode"){
+            body.classList.add("dark");
+          }
+
+		  modeToggle.addEventListener("click" , () =>{
+			modeToggle.classList.toggle("active");
+			body.classList.toggle("dark");
+
+			if(!body.classList.contains("dark")){
+				localStorage.setItem("mode" , "light-mode");
+			}else{
+				localStorage.setItem("mode" , "dark-mode");
+			}
+		  });
 
 window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY >0);
+	body.classList.toggle ("sticky", window.scrollY >0);
 });
 
 let menu = document.querySelector('#menu-icon');
@@ -35,6 +52,18 @@ var typed = new Typed(".typing-2",{
 	backSpeed: 60,
 	loop: true
 })
+
+	function SendMail(){
+		var params = {
+			from_name : document.getElementById("fullName").value,
+			email_id :document.getElementById("email_id").value,
+			subject :document.getElementById("subject").value,
+			message : document.getElementById("message").value
+		}
+		emailjs.send("service_ofrwue3", "template_faq0h1g", params).then(function (res) {
+			alert("Sucess! " + res.status);
+		})
+	}
 
 sr.reveal('.home-text',{delay:190, origin:'bottom'})
 
